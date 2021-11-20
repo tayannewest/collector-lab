6bae20f4f8d35753815eb9d266fd241af576324a
Tay West <tayanne.west@gmail.com>
2021-11-20 15:49:09 -0500

    add about template

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse("<h1>Alright kweeb, time to out yourself</h1>")
 
 def about(request):
-  return HttpResponse("<h1>How about this parasocial weirdo am I right lmao</h1>")
+  return render(request, "about.html")
