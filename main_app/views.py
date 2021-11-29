48fdf2813693948c7de42a7a71b71de0ef6198d0
Tay West <tayanne.west@gmail.com>
2021-11-28 20:25:12 -0500

    fix some styling

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from .models import Photocard, Style, Photo
@@ -19,10 +21,12 @@ class Home(LoginView):
 def about(request):
   return render(request, "about.html")
 
+@login_required
 def photocards_index(request):
-  photocards = Photocard.objects.all()
+  photocards = Photocard.objects.filter(user=request.user)
   return render(request, "photocards/index.html", { "photocards": photocards })
 
+@login_required
 def add_solo(request, photocard_id):
   form = SoloForm(request.POST)
   if form.is_valid():
@@ -31,13 +35,14 @@ def add_solo(request, photocard_id):
     new_solo.save()
   return redirect("photocards_detail", photocard_id=photocard_id)
 
+@login_required
 def photocards_detail(request, photocard_id):
   photocard = Photocard.objects.get(id=photocard_id)
   styles_photocard_doesnt_have = Style.objects.exclude( id__in = photocard.styles.all().values_list("id"))
   solo_form = SoloForm()
   return render(request, "photocards/detail.html", {"photocard": photocard, "solo_form": solo_form, "styles": styles_photocard_doesnt_have})
 
-class PhotocardCreate(CreateView):
+class PhotocardCreate(LoginRequiredMixin, CreateView):
   model = Photocard
   fields = ["name", "band", "era", "description"]
 
@@ -45,36 +50,38 @@ class PhotocardCreate(CreateView):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
-class PhotocardUpdate(UpdateView):
+class PhotocardUpdate(LoginRequiredMixin, UpdateView):
   model = Photocard
   fields = ["band", "era", "description"]
 
-class PhotocardDelete(DeleteView):
+class PhotocardDelete(LoginRequiredMixin, DeleteView):
   model = Photocard
   success_url = "/photocards/"
 
-class StyleCreate(CreateView):
+class StyleCreate(LoginRequiredMixin, CreateView):
   model = Style
   fields = "__all__"
 
-class StyleList(ListView):
+class StyleList(LoginRequiredMixin, ListView):
   model = Style
 
-class StyleDetail(DetailView):
+class StyleDetail(LoginRequiredMixin, DetailView):
   model = Style
 
-class StyleUpdate(UpdateView):
+class StyleUpdate(LoginRequiredMixin, UpdateView):
   model = Style
   fields = "__all__"
 
-class StyleDelete(DeleteView):
+class StyleDelete(LoginRequiredMixin, DeleteView):
   model = Style
   success_url = "/styles/"
 
+@login_required
 def assoc_style(request, photocard_id, style_id):
   Photocard.objects.get(id=photocard_id).styles.add(style_id)
   return redirect("photocards_detail", photocard_id=photocard_id)
 
+@login_required
 def add_photo(request, photocard_id):
   photo_file = request.FILES.get("photo-file", None)
   print(photo_file)
@@ -106,4 +113,3 @@ def signup(request):
   form = UserCreationForm()
   context = {"form": form, "error_message": error_message}
   return render(request, "signup.html", context)
-  
--- photocardcollector/settings.py
+++ photocardcollector/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+LOGIN_URL = "home"
+
 LOGIN_REDIRECT_URL = "photocards_index"
 
 LOGOUT_REDIRECT_URL = "home"
